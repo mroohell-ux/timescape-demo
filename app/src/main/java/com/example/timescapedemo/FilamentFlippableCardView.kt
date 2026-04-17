eb053feafebbc86abe5e28488f4e6b47b4867e37
mroohell-ux <commaenglish92@gmail.com>
2026-04-17 15:06:15 +0800

    Prevent text stretching by preserving snapshot aspect ratio

--- app/src/main/java/com/example/timescapedemo/FilamentFlippableCardView.kt
+++ app/src/main/java/com/example/timescapedemo/FilamentFlippableCardView.kt
@@ -49,10 +49,10 @@ class FilamentFlippableCardView @JvmOverloads constructor(
 
     private val surfaceView = SurfaceView(context)
     private val frontFaceView = ImageView(context).apply {
-        scaleType = ImageView.ScaleType.FIT_XY
+        scaleType = ImageView.ScaleType.FIT_CENTER
     }
     private val backFaceView = ImageView(context).apply {
-        scaleType = ImageView.ScaleType.FIT_XY
+        scaleType = ImageView.ScaleType.FIT_CENTER
     }
     private val frontShadeView = AndroidView(context).apply {
         setBackgroundColor(0xFF000000.toInt())
